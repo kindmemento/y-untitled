8bed3a96da18d9087dc49e4d0745f06843769bea
metindokmen <kindmemento@gmail.com>
2023-09-04 19:14:08 +0300

    use custom textfield widget for username and pwd fields on login screen

--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:y_mobile/components/textfield.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Login'),
-        ),
-        body: const Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text('Login to your account', style: TextStyle(fontSize: 24))],
-        )));
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Center(
+              child: Column(
+            children: [
+              const SizedBox(height: 25),
+              // TO BE REPLACE WITH ACTUAL LOGO
+              const Icon(Icons.lock, size: 100),
+              // PLACEHOLDER TEXT
+              Text(
+                'Welcome back',
+                style: TextStyle(color: Colors.grey[700], fontSize: 16),
+              ),
+
+              const SizedBox(height: 20),
+
+              CustomTextField(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false,
+              ),
+
+              const SizedBox(height: 20),
+
+              CustomTextField(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true,
+              ),
+
+              // forgot pwd?
+
+              // sign in button
+
+              // or continue with
+
+              // google OR apple sign in buttons
+
+              // Don't have an account? Register now
+            ],
+          )),
+        ));
   }
 }
