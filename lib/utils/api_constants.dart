7e972d5a243f3fece9cb777933eb4f3f61ef0663
metindokmen <kindmemento@gmail.com>
2023-09-06 10:03:34 +0300

    modify loginUrl, use interpolation

--- lib/utils/api_constants.dart
+++ lib/utils/api_constants.dart
@@ -1,5 +1,5 @@
 class ApiConstants {
   static const String baseUrl = "http://localhost:8080"; // TODO: Replace with actual baseUrl
   static const String registerUrl = '/register';
-  static const String loginUrl = '/login';
+  static const String loginUrl = '$baseUrl/login';
 }
