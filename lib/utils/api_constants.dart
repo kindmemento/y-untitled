2d06a15aa75e62baf579ef2bce82c77479d6efe0
metindokmen <kindmemento@gmail.com>
2023-09-06 07:58:42 +0300

    gather api constants in a separate class

---
+++ lib/utils/api_constants.dart
@@ -0,0 +1,5 @@
+class ApiConstants {
+  static const String baseUrl = "http://localhost:8080"; // TODO: Replace with actual baseUrl
+  static const String registerUrl = '/register';
+  static const String loginUrl = '/login';
+}
