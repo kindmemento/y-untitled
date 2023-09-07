f933db5824aa30890ed4ceab2f9185ef21dadae3
metindokmen <kindmemento@gmail.com>
2023-09-07 23:48:11 +0300

    make home screen stateful, add state to toggle between screens

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:y_mobile/screens/chat_screen.dart';
+import 'package:y_mobile/screens/discovery_screen.dart';
+import 'package:y_mobile/screens/footer.dart';
+import 'package:y_mobile/screens/profile_screen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Text("HOME"));
+    final List<Widget> _screens = [DiscoveryScreen(), ProfileScreen(), ChatScreen()];
+    return MaterialApp(
+        home: Scaffold(
+      body: _screens[_currentIndex],
+      bottomNavigationBar: AppFooter(
+        currentindex: _currentIndex,
+        onTabTapped: _onTabTapped,
+      ),
+    ));
   }
 }
