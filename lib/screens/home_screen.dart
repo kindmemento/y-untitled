a8cc34a4b0754d2c0a1f3b9f35023f9f617cd6cb
metindokmen <kindmemento@gmail.com>
2023-09-07 20:05:48 +0300

    wrap home screen inside safe area

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,6 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text("HOME");
+    return const SafeArea(child: Text("HOME"));
   }
 }
