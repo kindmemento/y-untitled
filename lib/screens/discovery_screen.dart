6d83b535398f7edf7120f48e2eab886fd5344fea
metindokmen <kindmemento@gmail.com>
2023-09-08 23:33:10 +0300

    use navbar as appbar in discovery screen

--- lib/screens/discovery_screen.dart
+++ lib/screens/discovery_screen.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:y_mobile/utils/navbar.dart';
 
 class DiscoveryScreen extends StatelessWidget {
   const DiscoveryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Discovery Screen')),
-      body: const Center(
+    return const Scaffold(
+      appBar: DiscoveryNavBar(),
+      body: Center(
         child: Text(
           'Discovery Screen',
           style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
