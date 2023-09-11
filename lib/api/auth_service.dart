741be3f70e98bfe382b153a95605d26f29c64b24
metindokmen <kindmemento@gmail.com>
2023-09-12 02:39:48 +0300

    decode jwt token, retrieve user info, set them in auth provider

--- lib/api/auth_service.dart
+++ lib/api/auth_service.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:y_mobile/api/api_service.dart';
 import 'package:y_mobile/utils/api_constants.dart';
 import 'package:y_mobile/utils/auth_provider.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 
 class AuthService {
   final ApiService _apiService = ApiService();
@@ -17,8 +18,26 @@ class AuthService {
       final requestData = {if (username != null) "username": username, if (email != null) "email": email, "password": password};
 
       final response = await _apiService.post(ApiConstants.loginUrl, data: requestData);
-
       _token = response.data['token'];
+
+      Map<String, dynamic> decodedToken = JwtDecoder.decode(_token!);
+
+      String? _username = decodedToken['username'];
+      String? _firstName = decodedToken['first_name'];
+      String? _lastName = decodedToken['last_name'];
+      String? _email = decodedToken['email'];
+      String? accountTypeString = decodedToken['account_type'];
+
+      AccountType? _accountType;
+      if (accountTypeString == 'personal') {
+        _accountType = AccountType.personal;
+      } else if (accountTypeString == 'team') {
+        _accountType = AccountType.team;
+      }
+
+      _authProvider.setToken(_token!);
+      _authProvider.setUserDetails(
+          username: _username, firstName: _firstName, lastName: _lastName, email: _email, accountType: _accountType);
       _authProvider.setLoggedIn(true);
     } catch (e) {
       throw ApiException.from(e);
