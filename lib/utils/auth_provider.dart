d193e7633937e54262e5d42096b0fcb4253b3e91
metindokmen <kindmemento@gmail.com>
2023-09-12 02:23:42 +0300

    add enum AccountType

--- lib/utils/auth_provider.dart
+++ lib/utils/auth_provider.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+enum AccountType { personal, team }
+
 class AuthProvider extends ChangeNotifier {
   String? _token;
   String? _username;
   String? _firstName;
   String? _lastName;
   String? _email;
+  AccountType? _accountType;
   bool _isLoggedIn = false;
 
   String? get token => _token;
@@ -13,20 +16,16 @@ class AuthProvider extends ChangeNotifier {
   String? get firstName => _firstName;
   String? get lastName => _lastName;
   String? get email => _email;
+  AccountType? get accountType => _accountType;
   bool get isLoggedIn => _isLoggedIn;
 
-  void setUser({
-    String? token,
-    String? username,
-    String? firstName,
-    String? lastName,
-    String? email,
-  }) {
+  void setUser({String? token, String? username, String? firstName, String? lastName, String? email, AccountType? accountType}) {
     _token = token;
     _username = username;
     _firstName = firstName;
     _lastName = lastName;
     _email = email;
+    _accountType = accountType;
     _isLoggedIn = true;
     notifyListeners();
   }
@@ -37,6 +36,7 @@ class AuthProvider extends ChangeNotifier {
     _firstName = null;
     _lastName = null;
     _email = null;
+    _accountType = null;
     _isLoggedIn = false;
     notifyListeners();
   }
