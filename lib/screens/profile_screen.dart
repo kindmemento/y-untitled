9a3ac6690532de0d62351fab39ae7a98b7b2c5b6
metindokmen <kindmemento@gmail.com>
2023-09-11 23:34:33 +0300

    implement a boilerplate layout for the profile screen with placeholders

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -6,11 +6,52 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile Screen')),
-      body: const Center(
-        child: Text(
-          'Profile Screen',
-          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15.0),
+              color: Colors.grey[300], // Added a color for visibility. Change as needed.
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(15.0),
+              child: Row(
+                children: [
+                  // First column
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text('Band Name / Full Name'), // placeholder
+                        SizedBox(height: 10),
+                        CircleAvatar(
+                          // Placeholder for the profile picture
+                          backgroundImage: NetworkImage('https://via.placeholder.com/100'),
+                          radius: 50.0,
+                        ),
+                        SizedBox(height: 10),
+                        Text('Number of Connections'),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 20), // Some spacing between columns
+                  // Second column
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text('Tools'),
+                        SizedBox(height: 10),
+                        Text('Location'),
+                        SizedBox(height: 10),
+                        Text('Level / Years of Experience'),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
